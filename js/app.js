7b1722bcd4a8ff79a4e17250b7fcccb8b1402e39
Kim Hue Mang 김희망 <105429536+KIMHUEMANG@users.noreply.github.com>
2022-06-15 15:21:12 +0900

    Dotori

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const clock = document.querySelector(".Date");
+const ReserveBtn = document.querySelector(".selfstudy .count_reserve .reserve_btn");
+const reserve = document.querySelector("progress");
+const reserveText = document.querySelector(".count_reserve_value");
+
+let ReserveValue = reserve.value;
+
+reserveText.innerText = `${ReserveValue} / 50`;
 
 function getclock(){
     const today = new Date();
@@ -11,3 +18,14 @@ function getclock(){
 }
 getclock();
 setInterval(getclock,100);
+
+function handleReserveBtn(){
+    if(ReserveValue < 50){
+        ReserveValue++;
+        reserveText.innerText = `${ReserveValue} / 50`;
+    }
+    else{
+        alert("자리가 다 찼습니다.");
+    }
+}
+ReserveBtn.addEventListener("click",handleReserveBtn);
